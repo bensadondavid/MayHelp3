07e598059ee1d43760a1ac2fc70a886b2fbcecc5
David Bensadon <bensadondavidn@gmail.com>
2024-09-01 02:45:42 +0300

    changes

--- client/src/Components/Carousel.jsx
+++ client/src/Components/Carousel.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 
 const Carousel = () => {
   const categories = [
-    { name: "TOURISTE", image: "url(touriste.jpg)", paragraphe : 'Voyager l’esprit tranquille ! MayHelp s’occupe de toutes vos démarches administratives pendant votre séjour. De la réservation de vos activités à l’organisation de vos formalités, profitez pleinement de votre voyage sans tracas administratifs.' },
-    { name: "SOLDAT", image: "url(soldatsimg.jpg)", paragraphe :'En tant que soldat, votre temps est précieux. MayHelp est là pour vous épauler dans vos démarches administratives, afin que vous puissiez vous concentrer sur l’essentiel. Qu’il s’agisse de documents officiels ou de services personnalisés, nous sommes à vos côtés.'},
-    { name: "ALYAH", image: "url(telaviv.jpg)", paragraphe : 'Faites de votre Alyah une expérience sereine et réussie. MayHelp vous guide à chaque étape, du visa aux démarches d’intégration. Nous simplifions votre installation en Israël pour que vous puissiez commencer cette nouvelle aventure en toute sérénité.' },
-    { name: "ADMINISTRATIF", image: "url(administratif.jpg)", paragraphe : 'Simplifiez votre quotidien avec MayHelp ! Que vous soyez résident ou expatrié, nous prenons en charge toutes vos formalités administratives, des documents officiels aux tâches courantes. Concentrez-vous sur l’essentiel, nous nous occupons du reste.' },
+    { name: "TOURISTE", image: "touriste.jpg", paragraphe : 'Voyager l’esprit tranquille ! MayHelp s’occupe de toutes vos démarches administratives pendant votre séjour. De la réservation de vos activités à l’organisation de vos formalités, profitez pleinement de votre voyage sans tracas administratifs.' },
+    { name: "SOLDAT", image: "soldatsimg.jpg", paragraphe :'En tant que soldat, votre temps est précieux. MayHelp est là pour vous épauler dans vos démarches administratives, afin que vous puissiez vous concentrer sur l’essentiel. Qu’il s’agisse de documents officiels ou de services personnalisés, nous sommes à vos côtés.'},
+    { name: "ALYAH", image: "telaviv.jpg", paragraphe : 'Faites de votre Alyah une expérience sereine et réussie. MayHelp vous guide à chaque étape, du visa aux démarches d’intégration. Nous simplifions votre installation en Israël pour que vous puissiez commencer cette nouvelle aventure en toute sérénité.' },
+    { name: "ADMINISTRATIF", image: "administratif.jpg", paragraphe : 'Simplifiez votre quotidien avec MayHelp ! Que vous soyez résident ou expatrié, nous prenons en charge toutes vos formalités administratives, des documents officiels aux tâches courantes. Concentrez-vous sur l’essentiel, nous nous occupons du reste.' },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -35,12 +35,13 @@ const Carousel = () => {
       </div>
       <div
         className="previous-category"
-        style={{ backgroundImage: categories[prevIndex].image }}
-      ></div>
+      >
+        <img src={categories[prevIndex].image} alt="" />
+      </div>
       <div
         className="category"
-        style={{ backgroundImage: categories[currentIndex].image }}
       >
+        <img src={categories[currentIndex].image} alt="" />
         <div className="sub-category">
           <p>{categories[currentIndex].paragraphe}</p>
         </div>
@@ -53,8 +54,9 @@ const Carousel = () => {
       </div>
       <div
         className="next-category"
-        style={{ backgroundImage: categories[nextIndex].image }}
-      ></div>
+      >
+        <img src={categories[nextIndex].image} alt="" />
+      </div>
     </div>
   );
 };
