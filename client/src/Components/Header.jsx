f72190af799e31b005694fd05dbb9a300ae3abbe
David Bensadon <bensadondavidn@gmail.com>
2024-09-04 23:14:26 +0300

    changes

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = ()=>{
 
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-    const location = useLocation();
     useEffect(()=>{
         const changeWidth = ()=>{
             setScreenWidth(window.innerWidth)
