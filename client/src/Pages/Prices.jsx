d22018d78da4bf5385717b7e9d1c6fea8772aee5
David Bensadon <bensadondavidn@gmail.com>
2024-09-25 18:18:38 +0300

    changes in prices

--- client/src/Pages/Prices.jsx
+++ client/src/Pages/Prices.jsx
@@ -1,47 +1,43 @@
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 import { useState } from 'react';
+import SpecialOffers from '../Components/Offers';
 
-const Prices = ()=>{
-
-
+const Prices = () => {
     const [visibleSections, setVisibleSections] = useState({});
 
     const toggleServices = (category) => {
-        setVisibleSections(prevState => ({
+        setVisibleSections((prevState) => ({
             ...prevState,
-            [category]: !prevState[category]
+            [category]: !prevState[category],
         }));
     };
 
-    return(
+    return (
         <>
             <Header />
             <div className='prices'>
+                <h1 style={{textAlign:'center', color:'white', fontFamily:'Montserrat', paddingBottom: '60px'}}>Nos Tarifs</h1>
                 <div className="services-table">
+
+                    {/* Services Administratifs */}
                     <div className="service-category">
-                        <h3>Services Administratifs (50 – 250 SHEKEL)</h3>
+                        <h3>Services Administratifs (50 – 250₪)</h3>
                         <ul className="service-list">
-                            <li>Gestion des documents officiels</li>
-                            <li>Obtention de permis de séjour, visas, et autres documents d’immigration</li>
-                            <li>Traduction de documents officiels</li>
-                            <li>Assistance pour remplir des formulaires administratifs</li>
+                            <li>Gestion des documents officiels (Arnona, Bituah Leoumi, etc.) : 80-130₪</li>
+                            <li>Obtention de permis de séjour, visas, et autres documents d’immigration : 120₪</li>
+                            <li>Traduction de documents officiels : 50₪</li>
+                            <li>Assistance pour remplir des formulaires administratifs : 70₪</li>
                             {visibleSections['administratifs'] && (
                                 <div className="more-services">
-                                    <li>Gaz, Électricité, Eau …</li>
-                                    <li>Accompagnement pour les démarches auprès des administrations locales</li>
-                                    <li>Gestion des amendes et contraventions</li>
-                                    <li>Vérification et paiement des amendes</li>
-                                    <li>Contestation de contraventions</li>
-                                    <li>Banques et finances</li>
-                                    <li>Ouverture de comptes bancaires</li>
-                                    <li>Assistance pour les prêts et crédits</li>
-                                    <li>Suivi des factures et paiements</li>
-                                    <li>Assurances</li>
-                                    <li>Souscription et gestion de contrats d’assurance (santé, habitation, voiture, etc.)</li>
-                                    <li>Rendez-vous administratifs</li>
-                                    <li>Prise de rendez-vous avec les services publics (misrad hapnim, etc.)</li>
-                                    <li>Accompagnement aux rendez-vous</li>
+                                    <li>Gaz, Électricité, Eau : 75₪</li>
+                                    <li>Accompagnement pour les démarches auprès des administrations locales : 80-130₪</li>
+                                    <li>Gestion des amendes et contraventions : 70-80₪</li>
+                                    <li>Ouverture de comptes bancaires (Jérusalem) : 100₪</li>
+                                    <li>Suivi des factures et paiements : 80₪</li>
+                                    <li>Souscription et gestion de contrats d’assurance (santé, habitation, voiture) : 80₪</li>
+                                    <li>Prise de rendez-vous avec les services publics : 60₪</li>
+                                    <li>Accompagnement aux rendez-vous (Jérusalem) : 100₪</li>
                                 </div>
                             )}
                             <li>
@@ -52,23 +48,19 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services de Voyage et Réservations */}
                     <div className="service-category">
-                        <h3>Services de Voyage et Réservations</h3>
+                        <h3>Services de Voyage et Réservations (80-150₪)</h3>
                         <ul className="service-list">
-                            <li>Réservations de voyage</li>
-                            <li>Logistique</li>
-                            <li>Conseil en itinéraire</li>
-                            <li>Assistance en cas d’imprévu</li>
+                            <li>Réservations de voyage (billets d’avion, train, bus) : 80₪</li>
+                            <li>Réservation de chambres d’hôtel : 80₪</li>
+                            <li>Logistique : Location de voitures, transferts aéroport (prix de la société)</li>
+                            <li>Conseil en itinéraire : 120₪</li>
                             {visibleSections['voyage'] && (
                                 <div className="more-services">
-                                    <li>Billets d’avion, train, et bus</li>
-                                    <li>Réservation de chambres d’hôtel ou d’hébergement</li>
-                                    <li>Location de voitures</li>
-                                    <li>Organisation de transferts aéroport/hôtel</li>
-                                    <li>Création d’itinéraires personnalisés pour voyages</li>
-                                    <li>Recommandations de sites touristiques</li>
-                                    <li>Réservation de dernière minute</li>
-                                    <li>Modification ou annulation de réservations</li>
+                                    <li>Recommandations de sites touristiques : 80₪</li>
+                                    <li>Réservation de dernière minute : 150₪</li>
+                                    <li>Modification ou annulation de réservations : 80₪</li>
                                 </div>
                             )}
                             <li>
@@ -79,21 +71,17 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services de Bien-être et Confort */}
                     <div className="service-category">
                         <h3>Services de Bien-être et Confort</h3>
                         <ul className="service-list">
-                            <li>Soutien pour les familles</li>
-                            <li>Services à domicile</li>
-                            <li>Bien-être personnel</li>
+                            <li>Soutien pour les familles (inscriptions scolaires) : 80₪</li>
+                            <li>Recherche de baby-sitters, nounous (prix du service + 15₪/heure)</li>
+                            <li>Ménage, repassage (prix du service + 15₪/heure)</li>
                             {visibleSections['bien-etre'] && (
                                 <div className="more-services">
-                                    <li>Assistance pour les inscriptions scolaires</li>
-                                    <li>Recherche et recommandation de baby-sitters, nounous, ou tuteurs</li>
-                                    <li>Ménage, repassage</li>
-                                    <li>Jardinage, entretien de piscine</li>
-                                    <li>Pet-sitting ou soins pour animaux de compagnie</li>
-                                    <li>Réservation de soins en spa, massages, coiffeurs, esthéticiennes</li>
-                                    <li>Organisation de séances de coaching ou de thérapie</li>
+                                    <li>Réservation de soins en spa, massages, coiffeurs : 80₪</li>
+                                    <li>Organisation de séances de coaching ou de thérapie : 80₪</li>
                                 </div>
                             )}
                             <li>
@@ -104,17 +92,15 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services d'Urgence et Assistance */}
                     <div className="service-category">
-                        <h3>Services d’Urgence et Assistance</h3>
+                        <h3>Services d’Urgence et Assistance (150-250₪)</h3>
                         <ul className="service-list">
-                            <li>Services d’urgence</li>
-                            <li>Assistance technique</li>
+                            <li>Assistance en cas de perte de documents (passeports, permis) : 250₪</li>
+                            <li>Gestion des situations d’urgence (domestiques, santé) : 250₪</li>
                             {visibleSections['urgence'] && (
                                 <div className="more-services">
-                                    <li>Assistance en cas de perte de documents (passeports, permis de conduire)</li>
-                                    <li>Gestion des situations d’urgence (incidents domestiques, santé)</li>
-                                    <li>Réparation d’appareils électroménagers</li>
-                                    <li>Intervention d’artisans (plombiers, électriciens, etc.)</li>
+                                    <li>Intervention d’artisans (plombiers, électriciens) : 150₪</li>
                                 </div>
                             )}
                             <li>
@@ -125,8 +111,9 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services Spécifiques pour les Soldats */}
                     <div className="service-category">
-                        <h3>Services Spécifiques pour les Soldats (100-400 SHEKEL)</h3>
+                        <h3>Services Spécifiques pour les Soldats (Gratuit ou jusqu’à 75 % de réduction)</h3>
                         <ul className="service-list">
                             <li>Aide logistique</li>
                             <li>Assistance administrative</li>
@@ -134,9 +121,9 @@ const Prices = ()=>{
                             {visibleSections['soldats'] && (
                                 <div className="more-services">
                                     <li>Réservation de transport et d’hébergement gratuit pour les soldats en service</li>
-                                    <li>Organisation de courses, courses alimentaires ou livraison de repas</li>
+                                    <li>Organisation de courses alimentaires ou livraison de repas</li>
                                     <li>Gestion des démarches administratives pendant la période de milouim</li>
-                                    <li>Soutien familial pour les soldats en service (gardes d’enfants, etc.)</li>
+                                    <li>Soutien familial pour les soldats (gardes d’enfants, etc.)</li>
                                 </div>
                             )}
                             <li>
@@ -147,18 +134,18 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services Événementiels et de Loisirs */}
                     <div className="service-category">
-                        <h3>Services Événementiels et de Loisirs</h3>
+                        <h3>Services Événementiels et de Loisirs (Devis à la demande)</h3>
                         <ul className="service-list">
-                            <li>Organisation d’événements</li>
-                            <li>Réservations pour loisirs</li>
+                            <li>Organisation d’événements (entreprise, séminaires)</li>
+                            <li>Réservations pour loisirs : 60₪</li>
                             <li>Cadeaux et surprises</li>
                             {visibleSections['evenementiel'] && (
                                 <div className="more-services">
-                                    <li>Événements d’entreprise, séminaires</li>
                                     <li>Réservation de restaurants, billets de concert, ou spectacles</li>
-                                    <li>Organisation de sorties en groupe (excursions, visites guidées)</li>
-                                    <li>Achat et livraison de cadeaux</li>
+                                    <li>Organisation de sorties en groupe (excursions, visites guidées) : 80₪</li>
+                                    <li>Achat et livraison de cadeaux (devis)</li>
                                     <li>Organisation de surprises pour occasions spéciales</li>
                                 </div>
                             )}
@@ -170,8 +157,9 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services Personnalisés */}
                     <div className="service-category">
-                        <h3>Services Personnalisés</h3>
+                        <h3>Services Personnalisés (Tarif sur demande)</h3>
                         <ul className="service-list">
                             <li>Conciergerie privée</li>
                             <li>Accompagnement personnalisé et disponible 24/7</li>
@@ -188,14 +176,14 @@ const Prices = ()=>{
                         </ul>
                     </div>
 
+                    {/* Services pour les Nouveaux Arrivants */}
                     <div className="service-category">
-                        <h3>Services pour les Nouveaux Arrivants</h3>
+                        <h3>Services pour les Nouveaux Arrivants (Tarif a la prestation)</h3>
                         <ul className="service-list">
-                            <li>Relocation</li>
-                            <li>Intégration</li>
+                            <li>Relocation : Recherche de logement, assistance pour l’installation</li>
+                            <li>Intégration : Cours de langue, orientation pour découvrir la ville et ses services</li>
                             {visibleSections['nouveaux-arrivants'] && (
                                 <div className="more-services">
-                                    <li>Recherche de logement</li>
                                     <li>Assistance pour l’installation (meubles, abonnements)</li>
                                     <li>Cours de langue</li>
                                     <li>Orientation pour découvrir la ville et ses services</li>
@@ -208,11 +196,13 @@ const Prices = ()=>{
                             </li>
                         </ul>
                     </div>
+
                 </div>
+                <SpecialOffers />
             </div>
             <Footer />
-            </>
-    )
-}
+        </>
+    );
+};
 
-export default Prices
+export default Prices;
